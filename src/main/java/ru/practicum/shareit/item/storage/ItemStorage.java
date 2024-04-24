bf8f1a4969c2b0e2d83179888ecb30e6694a4edc
Maria Nazarova <nazarovamariav@yandex.ru>
2024-04-25 00:27:52 +0300

    fix: ТЗ-13 code style

--- src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 public interface ItemStorage {
     Item create(Item item);
+
     Item findById(Integer itemId);
+
     Item update(Item item);
+
     List<Item> findAllItemsOfUser(Integer userId);
 
     List<Item> search(String lowerText);
